3c4483bdfec3ab1c2244fb3a5188e1b4f9b907c1
sneez1 <hugoeirmao@gmail.com>
2023-07-15 17:24:37 -0300

    exemplos

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//boolean
+//true .> verdaderiro
+//false .> falso
+
+const primeiroNumero = 5;
+const segundoNumero =  5;
+let cadastroAtivado = true;
+
+///console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
---
+++ type-string.js
@@ -0,0 +1,22 @@
+const texto1 = "Olá, mundo!"; 
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+
+const citacao = 'Meu nome é ';
+const meuNome ="Leonardo";
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
