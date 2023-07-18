d519620d59f44c771e5ba76abec0931b635a474e
sneez1 <hugoeirmao@gmail.com>
2023-07-18 13:49:23 -0300

    codigos var e conversoes

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+// boolean
+//const usuarioLogado = true
+//const contaPaga = false
+
+//truthy ou falsy
+
+//0 => false
+//1 => true
+
+//console.log(0 == false)
+//console.log("" == false)
+//console.log(1 == true)
+
+//null
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let text = alura;
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
